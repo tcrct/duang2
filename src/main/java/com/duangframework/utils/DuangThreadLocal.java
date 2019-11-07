e712c01d93a2c95c47c7e79172403f21b572bfbf
tcrct@163.com <tcrct790731>
2019-11-07 23:02:17 +0800

    待续开发

--- src/main/java/com/duangframework/utils/DuangThreadLocal.java
+++ src/main/java/com/duangframework/utils/DuangThreadLocal.java
@@ -1,7 +1,13 @@
 package com.duangframework.utils;
 
+import com.duangframework.kit.ToolsKit;
+import com.duangframework.mvc.dto.HeadDto;
+import com.duangframework.vtor.annotation.*;
+import com.duangframework.utils.DuangId;
+
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -35,7 +41,24 @@ public abstract  class DuangThreadLocal<T> {
 
 	private void reset(Thread thread, T value) {
 		if(duangThreadLocalMap.size() > MAX_THREAD_NUMBER) {
-			duangThreadLocalMap.clear();
+            boolean isHeadDtoObject = false;
+            for(Iterator<Map.Entry<Thread, T>> iterator = duangThreadLocalMap.entrySet().iterator(); iterator.hasNext();) {
+                Map.Entry<Thread, T> entry = iterator.next();
+                T threadLocalObject = entry.getValue();
+                // 如果是HeadDto，则判断存放时长是否大于3秒(默认3秒内返回)，如果是则清除
+                if(threadLocalObject instanceof HeadDto) {
+                    isHeadDtoObject = true;
+                    HeadDto headDto = (HeadDto)threadLocalObject;
+                    DuangId duangId = new DuangId(headDto.getRequestId());
+                    if ((System.currentTimeMillis() - duangId.getTime()) > 3000L) {
+                        iterator.remove();
+                    }
+                }
+            }
+            // 如果不是HeadDto对象则直接清除所有对象
+            if (!isHeadDtoObject) {
+                duangThreadLocalMap.clear();
+            }
 		}
 	}
 
