0c799d16b4f407c64857925b3f1cc8c3cb748554
admin@git.signetz.com <1b88ab6d>
2019-11-08 08:54:41 +0800

    持续开发

--- src/main/java/com/duangframework/utils/DuangThreadLocal.java
+++ src/main/java/com/duangframework/utils/DuangThreadLocal.java
@@ -2,6 +2,7 @@ package com.duangframework.utils;
 
 import com.duangframework.kit.ToolsKit;
 import com.duangframework.mvc.dto.HeadDto;
+import com.duangframework.mvc.http.enums.ConstEnums;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -39,27 +40,24 @@ public abstract  class DuangThreadLocal<T> {
 
 	private void reset(Thread thread, T value) {
 		if(duangThreadLocalMap.size() > MAX_THREAD_NUMBER) {
-            boolean isHeadDtoObject = false;
             for(Iterator<Map.Entry<Thread, T>> iterator = duangThreadLocalMap.entrySet().iterator(); iterator.hasNext();) {
                 Map.Entry<Thread, T> entry = iterator.next();
                 T threadLocalObject = entry.getValue();
                 // 如果是HeadDto，则判断存放时长是否大于3秒(默认3秒内返回)，如果是则清除
                 if(threadLocalObject instanceof HeadDto) {
-                    isHeadDtoObject = true;
                     HeadDto headDto = (HeadDto)threadLocalObject;
                     String requestId = headDto.getRequestId();
-                    if(ToolsKit.isEmpty(requestId)) {
-                    	continue;
-					}
-                    DuangId duangId = new DuangId(headDto.getRequestId());
-                    if ((System.currentTimeMillis() - duangId.getTime()) > 3000L) {
+                    if(ToolsKit.isNotEmpty(requestId)) {
+                        DuangId duangId = new DuangId(headDto.getRequestId());
+                        if ((System.currentTimeMillis() - duangId.getTime()) > 3000L) {
+                            iterator.remove();
+                        }
+                    }  else {
                         iterator.remove();
                     }
+                } else {
+                    iterator.remove();
                 }
-            }
-            // 如果不是HeadDto对象则直接清除所有对象
-            if (!isHeadDtoObject) {
-                duangThreadLocalMap.clear();
             }
 		}
 	}
